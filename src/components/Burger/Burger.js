4161904a21b39ba83f3b5371f2bc11d51c6bd24b
jhessiny <jhessinymattos@gmail.com>
2021-01-17 22:38:12 -0300

    burger and controls

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import classes from "./Burger.css";
+import BurguerIngredient from "./BurgerIngredient/BurgerIngredient";
+
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurguerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>;
+  }
+  return (
+    <div className={classes.Burger}>
+      <BurguerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurguerIngredient type="bread-bottom" />
+    </div>
+  );
+};
+
+export default burger;
